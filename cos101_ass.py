d1246a21fce85c5a38a513d0c1a906f995f59472
prince-omale <omale1002@gmail.com>
2024-12-05 01:28:47 +0100

    Commit message for the assignment

---
+++ cos101_ass.py
@@ -0,0 +1,15 @@
+okpa_list = ['bambar nut',
+             'maggie',
+             'palm oil',
+             'vegetable',
+             ]
+okpa_list.append('fish')
+print(okpa_list)
+
+numbers = list(range(1,11))
+total = sum(numbers)
+print("numbers:",numbers)
+print("sum of numbers:", total)
+
+for number in range(10,0,-1):
+    print(number)
